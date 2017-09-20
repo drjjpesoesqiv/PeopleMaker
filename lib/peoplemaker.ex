07a1a73812f29cd859d8d57650c34df429360b87
James Steinmetz <james.j.steinmetz@outlook.com>
2017-09-20 09:04:34 -0700

    initial project files

---
+++ lib/peoplemaker.ex
@@ -0,0 +1,18 @@
+defmodule Peoplemaker do
+  @moduledoc """
+  Documentation for Peoplemaker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Peoplemaker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
