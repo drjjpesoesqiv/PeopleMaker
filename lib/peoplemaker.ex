d44571c5dbb51fb37f78457b13b6b998dcf499bc
James Steinmetz <james.j.steinmetz@outlook.com>
2017-09-20 14:07:53 -0700

    supervisor / first select

--- lib/peoplemaker.ex
+++ lib/peoplemaker.ex
@@ -1,4 +1,28 @@
-defmodule Peoplemaker do
+defmodule Peoplemaker.Repo do
+  use Ecto.Repo, otp_app: :peoplemaker
+end
+
+defmodule Peoplemaker.People do
+  use Ecto.Schema
+  
+  schema "people" do
+    # field :id, :integer
+    field :first
+    field :last
+  end
+end
+
+defmodule Peoplemaker.App do
+  import Ecto.Query
+  alias Peoplemaker.Repo
+  alias Peoplemaker.People
+
+  def get_people do
+    People
+    |> order_by(:id)
+    |> limit(10)
+    |> Repo.all
+  end
   @moduledoc """
   Documentation for Peoplemaker.
   """
@@ -15,4 +39,5 @@ defmodule Peoplemaker do
   def hello do
     :world
   end
+  
 end
